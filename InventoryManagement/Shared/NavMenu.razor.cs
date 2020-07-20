5ef61f98a098829515ff0654fbc8010b79b0fa04
thebwood <brian.d.wood@gmail.com>
2020-07-20 19:16:56 -0400

    Cleaning up and adding navigation

---
+++ InventoryManagement/Shared/NavMenu.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Shared
+{
+    public partial class NavMenu : ComponentBase
+    {
+        [Parameter]
+        public EventCallback OnNavClicked { get; set; }
+
+    }
+}
