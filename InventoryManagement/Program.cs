1e451cf4c780f02f435331434a99f912d4c2f5ba
thebwood <brian.d.wood@gmail.com>
2020-07-22 00:47:19 -0400

    Updating Program.cs

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -16,10 +16,10 @@ namespace InventoryManagement
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddScoped(sp =>
+            builder.Services.AddScoped(sp => 
                 new HttpClient
                 {
-                    BaseAddress = new Uri("https://localhost:44349/")
+                    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
                 });
 
             builder.Services.AddSingleton<MoviesService>();
